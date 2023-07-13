860ee4a80a154894d0e79d782c722996e2fc0d89
pyutchnyambura <mkamunya1@gmail.com>
2023-07-13 09:38:16 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,3 +1,11 @@
 #include <stdio.h>
 /**
- * main - 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	ptintf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}	
